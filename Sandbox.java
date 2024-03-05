0d8f1d3245ca45016eb993e6889c085d53e520a4
EmmanuelKhumalo <123337700+EmmanuelKhumalo@users.noreply.github.com>
2024-03-05 03:35:50 +0200

    Add files via upload

---
+++ Sandbox.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+
+
+
+       int number1 = (int)(Math.random()*10);
+       int number2 = (int)(Math.random()*10);
+
+       Scanner input = new Scanner(System.in);
+
+       System.out.println("What is "+number1+"+"+number2+"?");
+
+       int answer = input.nextInt();
+
+       while(number1+number2 != answer){
+
+        System.out.println("Wrong answer try again");
+        System.out.println("What is "+number1+"+"+number2+"?");
+
+        answer = input.nextInt();
+
+       }
+
+       System.out.println("You are correct");
+
+
+  
+
+    }
+
+}
