e532985c7ff77ae5fc1998c301e5c93bcd1340f0
EmmanuelKhumalo <123337700+EmmanuelKhumalo@users.noreply.github.com>
2024-03-08 23:47:41 +0200

    Add files via upload

---
+++ Part01/05_Message.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        
+        Scanner input = new Scanner(System.in);
+        
+        String message = scanner.nextLine();
+        
+        //Print string
+        
+        System.out.println(message);
+        
+
+    }
+}
